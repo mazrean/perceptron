5140d817d00671922578675e9faf94d37e3ef86c
mazrean <shunsuke.w04@gmail.com>
2020-08-08 17:05:06 +0900

    日数を1/1からの日数に

--- read.py
+++ read.py
@@ -1,10 +1,11 @@
 import csv
-from datetime import datetime as dt
+import datetime as dt
 
 def strToDate(strDate):
-    time = dt.strptime(strDate, '%Y/%m/%d')
-    date = dt.date(time)
-    return date
+    time = dt.datetime.strptime(strDate, '%Y/%m/%d')
+    start = dt.date(time.year, 1, 1)
+    date = dt.date(time.year, time.month, time.day)
+    return (date - start).days + 1
 
 def read():
     f = open('./data/data.csv', encoding='shift_jis')
@@ -13,4 +14,4 @@ def read():
     data = list(map(lambda x: [strToDate(x[0]), float(x[1]), float(x[4]), int(x[7]), x[10]==0], reader))
     return data
 
-print(read()[0:5])
+print(read()[365:370])
